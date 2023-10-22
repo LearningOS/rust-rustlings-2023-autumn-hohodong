31989289aaab552f4a229ab4bfe01d448c9ccf75
hohodong <g15637665796@126.com>
2023-10-22 17:47:10 +0800

    complete

--- exercises/quiz1.rs
+++ exercises/quiz1.rs
@@ -13,10 +13,18 @@
 //
 // No hints this time ;)
 
-// I AM NOT DONE
+
 
 // Put your function here!
-// fn calculate_price_of_apples {
+fn calculate_price_of_apples(applenum : u32) -> u32{
+    let mut ret=0;
+    if applenum <= 40 {
+        ret = 2 * applenum;
+    } else {
+        ret = applenum;
+    }
+    ret
+}
 
 // Don't modify this function!
 #[test]
